6fcb53d445f0cdf6a111dab81640ff1a7f9b4435
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2025-03-12 17:11:02 -0400

    chore: Bring back v0 to the project as it is currently used (#144)

---
+++ tests/unit/charms/certificate_transfer_interface/v0/dummy_provider_charm/src/charm.py
@@ -0,0 +1,32 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from typing import Any
+
+from ops.charm import CharmBase, RelationJoinedEvent
+from ops.main import main
+
+from lib.charms.certificate_transfer_interface.v0.certificate_transfer import (
+    CertificateTransferProvides,
+)
+
+
+class DummyCertificateTransferProviderCharm(CharmBase):
+    def __init__(self, *args: Any):
+        super().__init__(*args)
+        self.certificate_transfer = CertificateTransferProvides(self, "certificates")
+        self.framework.observe(
+            self.on.certificates_relation_joined, self._on_certificates_relation_joined
+        )
+
+    def _on_certificates_relation_joined(self, event: RelationJoinedEvent):
+        certificate = "my certificate"
+        ca = "my CA certificate"
+        chain = ["certificate 1", "certificate 2"]
+        self.certificate_transfer.set_certificate(
+            certificate=certificate, ca=ca, chain=chain, relation_id=event.relation.id
+        )
+
+
+if __name__ == "__main__":
+    main(DummyCertificateTransferProviderCharm)
